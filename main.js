fd09a2ed3a15d6b16c61c064e9f66fae506b65e0
yebun91 <65695703+yebun91@users.noreply.github.com>
2021-04-16 14:36:51 +0900

    CRUD 완성

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-function templateHTML(title, list, body){
+const qs = require('querystring');
+
+function templateHTML(title, list, body, control){
   return `
   <!DOCTYPE html>
   <html lang="en">
@@ -33,7 +35,7 @@ function templateHTML(title, list, body){
       <h2>자유롭게 사용할 수 있는 게시판 입니다.</h2>
     </head>
     <div id="write">
-      <a href="http://">글쓰기</a>
+      <a href="/write">글쓰기</a>
     </div>
     <div id="list">
       <ul>
@@ -44,7 +46,8 @@ function templateHTML(title, list, body){
         </li>
       </ul>
     </div>
-    ${body}
+      ${body}
+      ${control}
     <div id="list">
       ${list}
     </div>
@@ -58,7 +61,7 @@ function templateHTML(title, list, body){
   </html>
   `;
 }
-function mainHTML(title, list){
+function mainHTML(title, list, body){
   return `
   <!DOCTYPE html>
   <html lang="en">
@@ -90,7 +93,8 @@ function mainHTML(title, list){
       <h2>자유롭게 사용할 수 있는 게시판 입니다.</h2>
     </head>
     <div id="write">
-      <a href="http://">글쓰기</a>
+      <a href="/write">글쓰기</a>
+      ${body}
     </div>
     <div id="list">
       ${list}
@@ -139,24 +143,111 @@ const app = http.createServer(function(request,response){
       fs.readdir('./data', function(err, filelist){
         const title = 'Simple Board';
         const list = temlpateList(filelist);
-        const template = mainHTML(title, list);
+        const template = mainHTML(title, list, ``);
         response.writeHead(200);
         response.end(template);
       });
       
     } else {
-      fs.readFile(`data/${title}`, `utf8`, function(err, descrption){
+      fs.readFile(`data/${title}`, `utf8`, function(err, description){
         fs.readdir('./data', function(err, filelist){
           const list = temlpateList(filelist);
           const template = templateHTML(title, list, 
-          `<div id = "text">
-            <p>${descrption}</p>
-          </div>`);
+          `<div id = "text"><p>${description}</p></div>`, 
+          `<a href = "/update?id=${title}">수정</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <input type="submit" value="삭제">
+          </form>`
+          );
           response.writeHead(200);
           response.end(template);
         });
       });
-    } 
+    }
+  } else if(pathname === '/write') {
+    fs.readdir('./data', function(err, filelist){
+      const title = 'WEB - write';
+      const list = temlpateList(filelist);
+      const template = mainHTML(title, list, `
+      <form action='/write_process' method="post">
+        <p><input type="text" name="title" placeholder="제목"></p>
+        <p>
+          <textarea name="description" placeholder="내용"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+          </form>
+      `,'');
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if(pathname === '/write_process') {
+    let body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      const post = qs.parse(body);
+      const title = post.title;
+      const description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.writeHead(302, {Location: `/?id=${encodeURI(title, description)}`});
+        response.end();
+      })
+    });
+  } else if(pathname ==='/update') {
+    fs.readdir('./data', function(err, filelist){
+      fs.readFile(`data/${title}`, 'utf8', function(err, description){
+        const list = temlpateList(filelist);
+        const template = templateHTML(title, list, 
+          `<form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="제목" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="내용">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `, 
+          `<a href="/update?id=${title}">글수정</a>`);
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else if(pathname === '/update_process'){
+    let body = '';
+    request.on('data', function(data){
+        body = body + data;
+    });
+    request.on('end', function(){
+        const post = qs.parse(body);
+        const id = post.id;
+        const title = post.title;
+        const description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${encodeURI(title, description)}`});
+            response.end();
+          })
+        });
+    });
+  } else if(pathname === '/delete_process'){
+    let body = '';
+    request.on('data', function(data){
+        body = body + data;
+    });
+    request.on('end', function(){
+        const post = qs.parse(body);
+        const id = post.id;
+        fs.unlink(`data/${id}`, function(err){
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        })
+    });  
   } else {
     response.writeHead(404);
     response.end('Not found');
